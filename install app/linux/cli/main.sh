fce9a5d91b05003b698505c3ac25f8c4f10c4ec6
hgc-02 <studenthiggins@gmail.com>
2026-08-12 13:42:48 +0800

    WIP cil app status:unpack cpio

--- install app/linux/cli/main.sh
+++ install app/linux/cli/main.sh
@@ -1,9 +1,16 @@
 echo 'path to file:'
 x=read
+y=$(pwd)
+z=
+
+
+if [ -f ]
 if [ -f "$x"]; then
     if [ -xwr "$x" ]; then
         if [ grep -q *img "$x" ]; then
-            python3 unpack_bootimg.py --boot_img "$x" --out output_dir/
+            python3 funtion/unpack_bootimg.py --boot_img "$x" --out "$y"
+            if [ grep -q *.cpio "$y" ]; then
+                cd 
         else 
             echo 'not img file'
         fi
