fa9978ec2b40f344fbdc600cefb9fd1463510061
HGC-02 <studenthiggins@gmail.com>
2026-08-16 19:30:03 +0800

    app a1 done

--- install app/linux/cli/main.sh
+++ install app/linux/cli/main.sh
@@ -20,12 +20,15 @@ if [ -f "$x"]; then
         if [ ls "$x"|grep -q *img  ]; then
             python3 funtion/unpack_bootimg.py --boot_img "$x" --out "$t"
             cd "$t"
-            if [[ ls|grep -q '.cpio' && find ramdisk.cpio]]; then
-            cpio -imv < $( find *.cpio )
-            
-
-                
-
+            if [ ls|grep -q '.cpio' ]; then
+                cpio -imv < $( find *.cpio )
+                mv init init.real
+                chmod +x init.real
+                cp init/init .
+                cp bin-for-app/busybox-arm64 /system/bin
+                chmod +x /system/bin/busybox-arm64
+                find . -print | cpio -o -H newc > ../my-archive.cpio
+                python3 funtion/repack_bootimg.py --boot_img ../ --out ../repack.img --ramdisk ../"$y"/my-archive.cpio
             fi
         else 
             echo 'not img file'
