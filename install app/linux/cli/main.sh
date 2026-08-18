f49ea0c9f9e5cf5951fd5b2f3be8c314949cfdbc
HGC-02 <studenthiggins@gmail.com>
2026-08-18 23:24:19 +0800

    b1

--- install app/linux/cli/main.sh
+++ install app/linux/cli/main.sh
@@ -15,20 +15,21 @@ if [ ! -d "$t" ]; then
     mkdir "$t"
 fi
 
-if [ -f "$x"]; then
-    if [ -xwr "$x" ]; then
-        if [ ls "$x"|grep -q *img  ]; then
+if [ -f "$x" ]; then
+    if [ -r "$x" ] && [ -w "$x" ] && [ -x "$x" ]; then
+        if [[ "$x" == *img* ]]; then
             python3 funtion/unpack_bootimg.py --boot_img "$x" --out "$t"
             cd "$t"
-            if [ ls|grep -q '.cpio' ]; then
-                cpio -imv < $( find *.cpio )
+            if  ls|grep -q '\.cpio' ; then
+                z=$( find *.cpio )
+                cpio -imv < "$z"
                 mv init init.real
                 chmod +x init.real
-                cp init/init .
-                cp bin-for-app/busybox-arm64 /system/bin
-                chmod +x /system/bin/busybox-arm64
+                cp ../init/init .
+                cp ../bin-for-app/busybox-arm64 system/bin
+                chmod +x system/bin/busybox-arm64
                 find . -print | cpio -o -H newc > ../my-archive.cpio
-                python3 funtion/repack_bootimg.py --boot_img ../ --out ../repack.img --ramdisk ../"$y"/my-archive.cpio
+                python3 funtion/repack_bootimg.py --boot_img "$x" --out ../repack.img --ramdisk "$y"/my-archive.cpio
             fi
         else 
             echo 'not img file'
